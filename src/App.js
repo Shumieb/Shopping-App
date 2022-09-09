82f91e932e846f4a9813ce0a5a0429730d6a56a6
Shumieb <shumiebc@gmail.com>
2022-09-09 19:52:11 +0100

    new clean up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import Home from './Pages/Home';
-import Footer from './SharedComponents/Footer';
-import NavBar from './SharedComponents/NavBar';
-import TopBanner from './SharedComponents/TopBanner';
-import './Styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <TopBanner/>
-      <NavBar/>
-      <Home/>
-      <Footer/>
+      App
     </div>
   );
 }
